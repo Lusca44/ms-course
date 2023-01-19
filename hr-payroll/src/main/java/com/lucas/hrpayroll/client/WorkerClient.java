c1678a459efef161e351ab8a547dfcdf3cb5d3d7
Lucas Santos <devlucas44@gmail.com>
2023-01-19 05:06:39 -0300

    implements rest template

---
+++ hr-payroll/src/main/java/com/lucas/hrpayroll/client/WorkerClient.java
@@ -0,0 +1,28 @@
+package com.lucas.hrpayroll.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.lucas.hrpayroll.entities.Worker;
+
+@Component
+public class WorkerClient {
+
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public Worker getWorkerById(Long workerId) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", String.valueOf(workerId));
+		return restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+	}
+	
+}
--- hr-payroll/src/main/java/com/lucas/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/lucas/hrpayroll/services/PaymentService.java
@@ -1,14 +1,21 @@
 package com.lucas.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lucas.hrpayroll.client.WorkerClient;
 import com.lucas.hrpayroll.entities.Payment;
+import com.lucas.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Autowired
+	private WorkerClient workerClient;
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Tobirama" , 200D, days);
+		Worker worker = workerClient.getWorkerById(workerId);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
-	
+
 }
